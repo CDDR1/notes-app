4ca93f497cf54b13ca7d024b7957d5ac32cd5fee
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-06-11 19:38:15 -0500

    Added dynamic date

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
+const getDate = () => {
+    const currentDate = new Date();
+    return `${currentDate.getMonth() + 1}/${currentDate.getDate()}/${currentDate.getFullYear()}`;
+};
+
 const Card = () => {
     return (
         <div className='card'>
@@ -13,7 +18,7 @@ const Card = () => {
                 value="Some value here"
             ></textarea>
             <div className="card__bottom">
-                <div className="card__date">06/10/2022</div>
+                <div className="card__date">{getDate()}</div>
                 <div className="card__delete-btn">
                     <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
                 </div>
