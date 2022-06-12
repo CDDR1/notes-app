d933ce56fb32f325bc02cfac35bedc58957b76b0
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-06-11 19:16:13 -0500

    Refactoring AddCard.jsx component. Saving progress...

--- src/AddCard.jsx
+++ src/AddCard.jsx
@@ -6,18 +6,9 @@ const getTextLength = () => {
     return document.querySelector(".card__textarea").value.length;  
 };
 
-const calculateRemainingChars = () => {
-    const textarea = document.querySelector(".card__textarea");
-    
-    // The first time the page loads, textarea will be equal to null. 
-    // In that case, 200 is returned to avoid an error. 
-    // When it's not null, we can make the calculation (total available chars - current chars).
-    return textarea === null ? 200 : 200 - getTextLength();
-};
-
 const AddCard = () => {
     let [currentChars, setCurrentChars] = useState(0);
-    let [remainingChars, setRemainingChars] = useState(200);
+    const maxLength = 200;
 
     return (
         <div className='card card--input'>
@@ -25,12 +16,12 @@ const AddCard = () => {
                 className='card__textarea'
                 name="card-text" 
                 id="card-text" 
-                maxLength={200}
+                maxLength={maxLength}
                 placeholder='Type to add a note...'
-                onChange={() => setCurrentChars({currentChars: getTextLength()})}
+                onChange={() => setCurrentChars(getTextLength())}
             ></textarea>
             <div className="card__bottom">
-                <div className="card__remainig-chars">{`${calculateRemainingChars()} Remaining`}</div>
+                <div className="card__remainig-chars">{maxLength - currentChars} Remaining</div>
                 <button 
                 className="card__save-btn"
                 onClick={() => console.log("clicked")}
